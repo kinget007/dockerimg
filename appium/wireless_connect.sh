f92c0433e82ef9824d5db16e30bfbbc9b025b53a
yilongyu <806833417qq.com>
2017-12-15 16:27:15 +0800

    update

--- appium/wireless_connect.sh
+++ appium/wireless_connect.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ ! -z "$ANDROID_DEVICE" ]; then
-    connected_devices=$(adb devices)
-    IFS=',' read -r -a array <<< "$ANDROID_DEVICE"
-    for i in "${!array[@]}"
-    do
-        array_device=$(echo ${array[$i]} | tr -d " ")
-        #string contains check
-        if [[ ${connected_devices} != *${array_device}* ]]; then
-            echo "Connecting to: ${array_device}"
-            adb connect ${array_device}
-            echo "Success!"
-        fi
-    done
-    #Give time to finish connection
-    sleep 1
-fi
+echo "connect device "
+# if [ ! -z "$ANDROID_DEVICE" ]; then
+#     connected_devices=$(adb devices)
+#     IFS=',' read -r -a array <<< "$ANDROID_DEVICE"
+#     for i in "${!array[@]}"
+#     do
+#         array_device=$(echo ${array[$i]} | tr -d " ")
+#         #string contains check
+#         if [[ ${connected_devices} != *${array_device}* ]]; then
+#             echo "Connecting to: ${array_device}"
+#             adb connect ${array_device}
+#             echo "Success!"
+#         fi
+#     done
+#     #Give time to finish connection
+#     sleep 1
+# fi
