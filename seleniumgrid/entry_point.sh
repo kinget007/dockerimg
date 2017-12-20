c3ed582655f9ffb192c4e7b9627f5cb4be64734a
yilongyu <806833417qq.com>
2017-12-20 09:05:47 +0800

    update

--- seleniumgrid/entry_point.sh
+++ seleniumgrid/entry_point.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CMD="xvfb-run java -jar /root/selenium-server-standalone-3.8.1.jar -port 4444 -role hub"
+CMD="xvfb-run java -jar /root/selenium-server-standalone-3.7.1.jar -port 4444 -role hub"
 pkill -x xvfb-run
 $CMD
