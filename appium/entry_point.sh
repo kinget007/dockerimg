7aa67d78a3d6aafe3e97d62bda1eaae0a1be8cfd
yilongyu <806833417qq.com>
2017-12-20 13:25:56 +0800

    update

--- appium/entry_point.sh
+++ appium/entry_point.sh
@@ -24,7 +24,7 @@ fi
 
 if [ ! -z "$CONNECT_TO_GRID" ]; then
   /root/generate_config.sh $NODE_CONFIG_JSON
-  CMD+=" --nodeconfig $NODE_CONFIG_JSON >/dev/null 2>&1"
+  CMD+=" --nodeconfig $NODE_CONFIG_JSON "
 fi
 
 pkill -x xvfb-run
