3b65108c6b3d6dce5009423b55b3c7863c1be7b5
yilongyu <806833417qq.com>
2017-12-20 09:41:21 +0800

    update

--- appium/entry_point.sh
+++ appium/entry_point.sh
@@ -4,9 +4,25 @@ NODE_CONFIG_JSON="/root/nodeconfig.json"
 #appium -U$ANDROID_DEVICE --port=9000 --bootstrap-port=10000 --chromedriver-port=20000 --selendroid-port=30000 --log={} --log-timestamp --no-reset
 CMD="xvfb-run appium -U$ANDROID_DEVICE --port=9000 --bootstrap-port=10000 --chromedriver-port=20000 --selendroid-port=30000 --chromedriver-executable=/root/chromedrivers/${CHROMEDRIVER_VERSION}"
 
-# if [ ! -z "$REMOTE_ADB" ]; then
-#     /root/wireless_connect.sh
-# fi
+if [ ! -z "$REMOTE_ADB" ]; then
+
+    if [ -z "$REMOTE_ADB_POLLING_SEC" ]; then
+        REMOTE_ADB_POLLING_SEC=10
+    fi
+
+    #to avoid immediate run
+    sleep ${REMOTE_ADB_POLLING_SEC}
+
+    if [ -z "$ANDROID_DEVICE" ]; then
+      echo "Connecting to: ${ANDROID_DEVICE}"
+      adb connect ${ANDROID_DEVICE}
+      #to avoid immediate run
+      sleep ${REMOTE_ADB_POLLING_SEC}
+      device=$(adb devices)
+      echo "Devices found: ${device}"
+      echo "Success!"
+    fi
+fi
 
 if [ ! -z "$CONNECT_TO_GRID" ]; then
   /root/generate_config.sh $NODE_CONFIG_JSON
